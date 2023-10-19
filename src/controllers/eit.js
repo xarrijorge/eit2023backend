50ebfcb320d2fe5a07315043d538182f22ab0a14
Xarri Jorge <kodinglife@gmail.com>
2023-10-19 11:53:12 +0000

    Added additional routes

--- src/controllers/eit.js
+++ src/controllers/eit.js
@@ -5,15 +5,44 @@ import Supabase_Client from "../utils/dbConnection.js";
 
 // import data from "../eitsdata.js"
 
-const { data: users, error } = await Supabase_Client 
+const { data: users, error } = await Supabase_Client
   .from('users')
   .select()
 
 
+const { data: skills, error } = await Supabase_Client
+  .from('skills')
+  .select()
 
-eitRouter.get('/eits', (req, res) =>{
-    // const person = Math.floor(Math.random() * 51)
-    res.json(users)
+const { data: roles, error } = await Supabase_Client
+  .from('roles')
+  .select()
+
+const { data: tskills, error } = await Supabase_Client
+  .from('tskills')
+  .select()
+
+
+// Routes below
+
+eitRouter.get('/eits', (req, res) => {
+  // const person = Math.floor(Math.random() * 51)
+  res.json(users)
+})
+
+eitRouter.get('/skills', (req, res) => {
+  // const person = Math.floor(Math.random() * 51)
+  res.json(skills)
+})
+
+eitRouter.get('/roles', (req, res) => {
+  // const person = Math.floor(Math.random() * 51)
+  res.json(roles)
+})
+
+eitRouter.get('/tskills', (req, res) => {
+  // const person = Math.floor(Math.random() * 51)
+  res.json(tskills)
 })
 
 
