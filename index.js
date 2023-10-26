6ca90b3e21f13f8058df307cc03fc82074152359
perismuthoni <muthonimuriuki22@gmail.com>
2023-10-26 21:41:01 +0000

    project progress

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
-import 'dotenv/config'
-import express from "express"
-const app = express()
+import 'dotenv/config';
+import express from 'express';
+const app = express();
 
-import cors from "cors"
-import bodyParser from "body-parser"
-import accessControl from "./src/utils/accessControl.js"
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import accessControl from './src/utils/accessControl.js';
 
-import eitRouter from "./src/controllers/eit.js"
+import eitRouter from './src/controllers/eit.js';
 
-app.use(cors())
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(accessControl)
+app.use(accessControl);
 
-app.get('/', (req, res) =>  res.send("Hello there, Peris Muthoni Muriuki!") )
-app.get('/eits', eitRouter)
-app.get('/skills', eitRouter)
-app.get('/roles', eitRouter)
-app.get('/tskills', eitRouter)
+app.get('/', (req, res) => res.send('Hello there, Peris Muthoni Muriuki!'));
+app.get('/eits', eitRouter);
+app.get('/skills', eitRouter);
+app.get('/roles', eitRouter);
+app.get('/tskills', eitRouter);
+app.get('/abscrow_store', eitRouter);
+app.get('/abscrow_users', eitRouter);
+app.get('/abscrow_store/user/:email', eitRouter);
 
 // app.get('/skills', skillRouter)
 // app.post('/skills', skillRouter)
 // app.post('/login', authRouter)
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-    console.log('Server listening on PORT', PORT)
-})
+  console.log('Server listening on PORT', PORT);
+});
--- src/controllers/eit.js
+++ src/controllers/eit.js
@@ -1,49 +1,72 @@
-import express from "express";
-const eitRouter = express.Router()
-import Supabase_Client from "../utils/dbConnection.js";
-
+import express from 'express';
+const eitRouter = express.Router();
+import Supabase_Client from '../utils/dbConnection.js';
 
 // import data from "../eitsdata.js"
 
-const { data: users, error } = await Supabase_Client
-  .from('users')
-  .select()
+const { data: users } = await Supabase_Client.from('users').select();
 
+const { data: skills } = await Supabase_Client.from('skills').select();
 
-const { data: skills, error } = await Supabase_Client
-  .from('skills')
-  .select()
+const { data: roles } = await Supabase_Client.from('roles').select();
 
-const { data: roles, error } = await Supabase_Client
-  .from('roles')
-  .select()
+const { data: tskills } = await Supabase_Client.from('tskills').select();
 
-const { data: tskills, error } = await Supabase_Client
-  .from('tskills')
-  .select()
-
+const { data: abscrow_store } = await Supabase_Client.from(
+  'abscrow_store'
+).select();
+const { data: abscrow_users } = await Supabase_Client.from(
+  'abscrow_users'
+).select();
 
 // Routes below
 
 eitRouter.get('/eits', (req, res) => {
   // const person = Math.floor(Math.random() * 51)
-  res.json(users)
-})
+  res.json(users);
+});
 
 eitRouter.get('/skills', (req, res) => {
   // const person = Math.floor(Math.random() * 51)
-  res.json(skills)
-})
+  res.json(skills);
+});
 
 eitRouter.get('/roles', (req, res) => {
   // const person = Math.floor(Math.random() * 51)
-  res.json(roles)
-})
+  res.json(roles);
+});
 
 eitRouter.get('/tskills', (req, res) => {
   // const person = Math.floor(Math.random() * 51)
-  res.json(tskills)
-})
+  res.json(tskills);
+});
+
+eitRouter.get('/abscrow_store', (req, res) => {
+  // const person = Math.floor(Math.random() * 51)
+  res.json(abscrow_store);
+});
+
+eitRouter.get('/abscrow_users', (req, res) => {
+  // const person = Math.floor(Math.random() * 51)
+  res.json(abscrow_users);
+});
+
+eitRouter.get('/abscrow_store/user/:email', (req, res) => {
+  const userEmail = req.params.email; // Get the email parameter from the URL
+
+  // Search for the user in the abscrow_store array by email
+  const user = abscrow_store.find(user => user.email === userEmail);
+
+  if (user) {
+    res.json(user); // User found and returned as JSON
+  } else {
+    res.status(404).json({ message: 'User not found' }); // User not found, respond with a 404 status
+  }
+});
 
 
-export default eitRouter 
+
+
+
+
+export default eitRouter;
