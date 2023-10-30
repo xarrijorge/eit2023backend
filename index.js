b168ca03c05620b1fc6589c27324a3dbcf6af0ef
Xarri Jorge <kodinglife@gmail.com>
2023-10-30 21:36:27 +0000

    Initial auth workflow

--- index.js
+++ index.js
@@ -1,28 +1,99 @@
-import 'dotenv/config';
-import express from 'express';
-const app = express();
+import 'dotenv/config'
+import express from "express"
+import expressSession from "express-session"
+import passport from "passport"
+import Auth0Strategy from "passport-auth0"
+
+
+const app = express()
 
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import accessControl from './src/utils/accessControl.js';
 
-import eitRouter from './src/controllers/eit.js';
+import eitRouter from "./src/controllers/eit.js"
+import authRouter from "./src/controllers/auth.js"
 
-app.use(cors());
+
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT, () => {
+    console.log('Server listening on PORT', PORT)
+})
+
+/**
+ * Session Configuration (New!)
+ */
+
+const session = {
+    secret: process.env.SESSION_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: false
+};
+
+if (app.get("env") === "production") {
+    // Serve secure cookies, requires HTTPS
+    session.cookie.secure = true;
+}
+
+/**
+ * Passport Configuration (New!)
+ */
+const strategy = new Auth0Strategy(
+    {
+        domain: process.env.AUTH0_DOMAIN,
+        clientID: process.env.AUTH0_CLIENT_ID,
+        clientSecret: process.env.AUTH0_CLIENT_SECRET,
+        callbackURL: process.env.AUTH0_CALLBACK_URL
+    },
+    function (accessToken, refreshToken, extraParams, profile, done) {
+        /**
+         * Access tokens are used to authorize users to an API
+         * (resource server)
+         * accessToken is the token to call the Auth0 API
+         * or a secured third-party API
+         * extraParams.id_token has the JSON Web Token
+         * profile has all the information from the user
+         */
+        return done(null, profile);
+    }
+);
+
+
+
+/**
+ *  App Configuration
+ */
+
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(accessControl);
 
-app.get('/', (req, res) => res.send('Hello there, Peris Muthoni Muriuki!'));
-app.get('/eits', eitRouter);
-app.get('/skills', eitRouter);
-app.get('/roles', eitRouter);
-app.get('/tskills', eitRouter);
-app.get('/abscrow_store', eitRouter);
-app.get('/abscrow_users', eitRouter);
-app.get('/abscrow_users/user/:email', eitRouter);
-app.post('/api/shop', eitRouter);
+app.use(expressSession(session));
+
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+
+app.get('/login', authRouter)
+
+app.get('/', (req, res) => res.send("Hello there, Peris Muthoni Muriuki!"))
+app.get('/eits', eitRouter)
+app.get('/skills', eitRouter)
+app.get('/roles', eitRouter)
+app.get('/tskills', eitRouter)
+
 // app.get('/skills', skillRouter)
 // app.post('/skills', skillRouter)
 // app.post('/login', authRouter)
